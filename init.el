26a4f5c6c5335f5bd27d80fc74ae1004099d9510
Andrew Stewart <andrew@stwrt.ca>
2017-07-07 22:01:28 -0700

    Add evil-mode

--- init.el
+++ init.el
@@ -12,12 +12,16 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
+(use-package evil
+  :ensure t
+  :init (evil-mode 1))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (use-package))))
+ '(package-selected-packages (quote (evil use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
