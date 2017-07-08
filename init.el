359a82961e8650f7182beb3a2532b56753fa1f63
Andrew Stewart <andrew@stwrt.ca>
2017-07-07 22:01:04 -0700

    Add self-bootstrapping use-package

--- init.el
+++ init.el
@@ -6,3 +6,21 @@
 
 (setq package-enable-at-startup nil)
 (package-initialize)
+
+;; bootstrap use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
