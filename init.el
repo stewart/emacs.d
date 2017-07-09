28c4da3ed27f4c1cf51caacde87dc3c7e1ac2a89
Andrew Stewart <andrew@stwrt.ca>
2017-07-08 20:01:27 -0700

    Configure backups, auto saving

--- init.el
+++ init.el
@@ -19,6 +19,20 @@
 ;; disable toolbar
 (tool-bar-mode -1)
 
+;; configure backups and autosaves
+(let ((backup-dir (expand-file-name "backups" user-emacs-directory))
+      (auto-saves-dir (expand-file-name "auto-saves" user-emacs-directory)))
+  (setq backup-directory-alist `(("." . ,backup-dir))
+        auto-save-file-name-transforms `((".*" ,auto-saves-dir t))
+        auto-save-list-file-prefix (concat auto-saves-dir ".saves-")))
+
+(setq
+  backup-by-copying t
+  delete-old-versions t
+  version-control t
+  kept-new-versions 6
+  kept-old-versions 2)
+
 (use-package evil
   :ensure t
   :init (evil-mode 1))
