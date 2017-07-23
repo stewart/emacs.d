57565bb906204d125a95e41f70ddd8d27552c1ef
Andrew Stewart <andrew@stwrt.ca>
2017-07-22 17:21:29 -0700

    Add flycheck

--- init.el
+++ init.el
@@ -51,3 +51,4 @@
 (require 'init-evil)
 (require 'init-org)
 (require 'init-helm)
+(require 'init-flycheck)
---
+++ lisp/init-flycheck.el
@@ -0,0 +1,7 @@
+;;; init-flycheck.el --- Configures flycheck
+
+(require-package 'flycheck)
+(global-flycheck-mode)
+
+(provide 'init-flycheck)
+;;; init-flycheck.el ends here
