529fc65d48f1b8fd8d8b61fcae998622754fa7ab
Andrew Stewart <andrew@stwrt.ca>
2017-07-22 17:19:37 -0700

    Add evil-exchange and evil-surround

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -3,5 +3,11 @@
 (require-package 'evil)
 (evil-mode 1)
 
+(require-package 'evil-surround)
+(global-evil-surround-mode 1)
+
+(require-package 'evil-exchange)
+(evil-exchange-install)
+
 (provide 'init-evil)
 ;;; init-evil.el ends here
