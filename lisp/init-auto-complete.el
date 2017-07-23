3484356eef720b0666e6a88849f28cef8e2da47a
Andrew Stewart <andrew@stwrt.ca>
2017-07-22 23:26:58 -0700

    Add way to push local company backends

--- lisp/init-auto-complete.el
+++ lisp/init-auto-complete.el
@@ -6,5 +6,10 @@
 (after-load 'company
   (diminish 'company-mode))
 
+(defun stwrt/local-push-company-backend (backend)
+    "Add BACKEND to a buffer-local version of `company-backends'."
+    (set (make-local-variable 'company-backends)
+         (append (list backend) company-backends)))
+
 (provide 'init-auto-complete)
 ;;; init-auto-complete.el ends here
